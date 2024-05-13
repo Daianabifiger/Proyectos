33a3f2c3a58d2e660d884348afb39dcd9d2e80d0
Daianabifiger <dbifiger@gmail.com>
2024-05-13 16:24:19 -0300

    Clase6

---
+++ Clase 06/App1Interfaces/Ijugador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1Interfaces
+{
+    public interface Ijugador
+    {
+        void correr();
+        void regatear();
+        void defender();
+        void rematar();
+        void colocarPase();
+        void soportarEmbate();
+    }
+}
---
+++ Clase 06/App1Interfaces/Ronaldo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1Interfaces
+{
+    internal class Ronaldo: jugador , Ijugador
+    {
+        public Ronaldo(int velocidad, int tiro, int regate, int defensa, int pase, int fisico) : base (velocidad, tiro, regate, defensa, pase, fisico)
+        {
+
+        }
+
+        public void colocarPase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void correr()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void defender()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void regatear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void rematar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void soportarEmbate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
